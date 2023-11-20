5d60c6cf7199d124abe6a3fe6a3d21c50a93103b
irteza-sidd <140733000+irteza-sidd@users.noreply.github.com>
2023-11-20 19:02:51 +0500

    user and video model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+const { app } = require("./app");
 const { connectDB } = require("./db/db");
 
 require("dotenv").config("./env");
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed", error);
+  });
